293a20ca423e835ff528abbe1a50f08bb25ce290
tuananhc <ctanh2001@gmail.com>
2021-08-12 09:34:55 +0700

    added starting and ending node

--- src/App.js
+++ src/App.js
@@ -2,31 +2,16 @@ import './App.css';
 import anime from 'animejs/lib/anime.es.js';
 import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core'
-import { render } from '@testing-library/react';
-
-const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
-
-  const updateMousePosition = ev => {
-    setMousePosition({ x: ev.clientX, y: ev.clientY });
-  };
-
-  useEffect(() => {
-    window.addEventListener("mousemove", updateMousePosition);
-
-    return () => window.removeEventListener("mousemove", updateMousePosition);
-  }, []);
-
-  return mousePosition;
-};
 
 function App() {
 
-  const [grid, setGrid] = useState(createGrid())
-  const [startingNode, setStartingNode] = useState()
-  const [endingNode, setEndingNode] = useState()
-  const { x, y } = useMousePosition()
   const blankGrid = createGrid()
+  const [grid, setGrid] = useState(createGrid())
+  const [isMousePressed, setIsMousePressed] = useState(false)
+  const [startingNode, setStartingNode] = useState([Math.floor((window.innerHeight - 200) / 40), Math.floor(window.innerWidth / 80)])
+  const [endingNode, setEndingNode] = useState([Math.floor((window.innerHeight - 200) / 40), Math.floor(window.innerWidth * 3 / 80)])
+  const [isMovingStart, setIsMovingStart] = useState(false)
+  const [isMovingEnd, setIsMovingEnd] = useState(false)
 
   function createGrid() {
     var grids = []
@@ -53,11 +38,7 @@ function App() {
           return (
             <div style={{ display: 'flex', flexDirection: 'row' }}>
               {row.map(node => {
-                if (node.isWall) {
-                  return createWall(node.col, node.row)
-                } else {
-                  return createNode(node.col, node.row)
-                }
+                return createNode(node)
               })}
             </div>
           )
@@ -66,60 +47,95 @@ function App() {
     )
   }
 
-  function createWall(col, row) {
-    return (
-      <div
-        id={"node".concat(col * row + col)}
-        style={{ width: 20, height: 20, backgroundColor: 'black' }}
-        onMouseDown={() => {
-          var newGrid = [...grid]
-          var node = newGrid[row][col]
-          node.isWall = !node.isWall
-          newGrid[row][col] = node
-          setGrid(newGrid)
-          setIsMousePressed(true)
-        }}
-        onMouseEnter={() => {
-          if (isMousePressed) {
-            var newGrid = [...grid]
-            var node = newGrid[row][col]
-            node.isWall = !node.isWall
-            newGrid[row][col] = node
-            setGrid(newGrid)
-          }
-        }}
-        onMouseUp={() => setIsMousePressed(false)}
-      />
-    )
-  }
-
-  const [isMousePressed, setIsMousePressed] = useState(false)
-
-  function createNode(col, row) {
+  function createNode(node) {
     return (
       <div
         className='node'
-        id={"node".concat(col * row + col)}
-        style={{ width: 18, height: 18, border: '1px solid #7DEDFF' }}
+        id={"node".concat(Math.floor(window.innerWidth / 20) * node.row + node.col)}
+        style={{ width: 18, height: 18, border: '1px solid #7DEDFF', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
         onMouseDown={() => {
+          if ((node.row === startingNode[0] && node.col === startingNode[1])) {
+            setIsMovingStart(true)
+            return
+          }
+          if ((node.row === endingNode[0] && node.col === endingNode[1])) {
+            setIsMovingEnd(true)
+            return
+          }
           var newGrid = [...grid]
-          var node = newGrid[row][col]
-          node.isWall = !node.isWall
-          newGrid[row][col] = node
+          var curNode = newGrid[node.row][node.col]
+          curNode.isWall = !node.isWall
+          newGrid[node.row][node.col] = curNode
           setGrid(newGrid)
           setIsMousePressed(true)
         }}
         onMouseEnter={() => {
+          if (isMovingStart) {
+            if ((node.row === endingNode[0] && node.col === endingNode[1])) { return }
+            if (node.isWall) {
+              var newGrid = [...grid]
+              var curNode = newGrid[node.row][node.col]
+              curNode.isWall = !node.isWall
+              newGrid[node.row][node.col] = curNode
+              setGrid(newGrid)
+            }
+            setStartingNode([node.row, node.col])
+
+          }
+          if (isMovingEnd) {
+            if ((node.row === startingNode[0] && node.col === startingNode[1])) { return }
+            if (node.isWall) {
+              var newGrid = [...grid]
+              var curNode = newGrid[node.row][node.col]
+              curNode.isWall = !node.isWall
+              newGrid[node.row][node.col] = curNode
+              setGrid(newGrid)
+            }
+            setEndingNode([node.row, node.col])
+
+          }
           if (isMousePressed) {
             var newGrid = [...grid]
-            var node = newGrid[row][col]
-            node.isWall = !node.isWall
-            newGrid[row][col] = node
+            var curNode = newGrid[node.row][node.col]
+            curNode.isWall = !node.isWall
+            newGrid[node.row][node.col] = curNode
             setGrid(newGrid)
           }
         }}
-        onMouseUp={() => setIsMousePressed(false)}
-      />
+        onMouseUp={() => {
+          if (isMovingStart) {
+            setIsMovingStart(false)
+          }
+          if (isMovingEnd) {
+            setIsMovingEnd(false)
+          }
+          if (isMousePressed) {
+            setIsMousePressed(false)
+          }
+        }}
+      >
+        {(node.isWall) ? (
+          <div style={{ width: 18, height: 18, backgroundColor: 'black' }} />
+        ) : (
+          <></>
+        )}
+        {(node.row === startingNode[0] && node.col === startingNode[1]) ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ width: 18, height: 18, borderRadius: 18, backgroundColor: 'black' }} />
+            <div style={{ width: 12, height: 12, borderRadius: 12, backgroundColor: 'white', position: 'absolute' }} />
+          </div>
+        ) : (
+          <></>
+        )}
+        {(node.row === endingNode[0] && node.col === endingNode[1]) ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ width: 18, height: 18, borderRadius: 18, backgroundColor: 'red' }} />
+            <div style={{ width: 12, height: 12, borderRadius: 12, backgroundColor: 'green', position: 'absolute' }} />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     )
   }
 
@@ -161,11 +177,12 @@ function App() {
           style={{ width: 100 }}
           onClick={() => {
             setGrid(blankGrid)
+            setStartingNode([Math.floor((window.innerHeight - 200) / 40), Math.floor(window.innerWidth / 80)])
+            setEndingNode([Math.floor((window.innerHeight - 200) / 40), Math.floor(window.innerWidth * 3 / 80)])
           }}
         >
           Reset
         </Button>
-        {x} : {y}
       </div>
       <div>
         {renderGrid(grid)}
